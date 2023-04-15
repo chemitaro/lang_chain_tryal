2b4e78c7f59f0d52ba5bbdcf5bdb68050233a373
chemitaro <iwasi_44@hotmail.com>
2023-04-15 18:07:14 +0900

    bing実装

--- scripts/mrkl_chat.py
+++ scripts/mrkl_chat.py
@@ -1,5 +1,5 @@
 from langchain import OpenAI, LLMMathChain, SerpAPIWrapper, WikipediaAPIWrapper
-from langchain.utilities import BashProcess
+from langchain.utilities import BashProcess, BingSearchAPIWrapper
 from langchain.agents import initialize_agent, Tool
 from langchain.agents import AgentType
 from langchain.chat_models import ChatOpenAI
@@ -10,6 +10,7 @@ search = SerpAPIWrapper()
 llm_math_chain = LLMMathChain(llm=llm1, verbose=True)
 wiki = WikipediaAPIWrapper()
 bash = BashProcess()
+bing = BingSearchAPIWrapper()
 
 tools = [
     Tool(
@@ -30,10 +31,15 @@ tools = [
     Tool(
         name="Bash",
         func=bash.run,
-        description="useful for when you need to answer questions involving the generation of bash commands, an LLM can often be employed. A typical scenario is enabling the LLM to interact with your local file system. To facilitate this, we offer a convenient utility for executing bash commands with ease."
+        description="It is used to generate and execute bash commands. A typical scenario is to allow the LLM to interact with the local file system. For this purpose, we provide a convenient utility to easily execute bash commands."
+    ),
+    Tool(
+        name="Bing",
+        func=bing.run,
+        description="useful for when you need to answer questions about current events. You can ask questions in natural language."
     )
 ]
 
 mrkl = initialize_agent(tools, llm, agent=AgentType.CHAT_ZERO_SHOT_REACT_DESCRIPTION, verbose=True)
 
-output =mrkl.run("/Users/iwasawayuuta/workspace/python/lang_chain_trialのディレクトリに hoge.txt を作成して その中に hoge と書き込んでください")
+output =mrkl.run("今日の重要なニュースを教えてください。")
